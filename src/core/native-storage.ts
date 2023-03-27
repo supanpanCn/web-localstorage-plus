969fe155a1c85e9ebe026c09ded686a2c1ae28b2
spp <weidian@supanpandeMac-mini.local>
2023-03-27 10:37:57 +0800

    修复多标签页数据不同步问题

--- src/core/native-storage.ts
+++ src/core/native-storage.ts
@@ -18,51 +18,66 @@ export interface RemoveParams {
 
 export default function (rootName: string) {
   const localStorage = window.WEB_STORAGE_USE_LOCAL_STORAGE();
-  const storage = JSON.parse(localStorage.getItem(rootName) || "{}");
-  function _update() {
+  const load = () => JSON.parse(localStorage.getItem(rootName) || "{}");
+  function _update(storage: any) {
     localStorage.setItem(rootName, JSON.stringify(storage));
   }
-  function _createSpace(namespace?:string){
-    if(namespace){
-        storage[namespace] = storage[namespace] || {}
-        return storage[namespace]
+  function _createSpace(storage: any, namespace?: string) {
+    if (namespace) {
+      storage[namespace] = storage[namespace] || {};
+      return storage[namespace];
     }
-    return storage
+    return storage;
   }
-  return {
-    storage:cloneDeep(storage),
-    methods: {
-      getItem(params: GetParams) {
-        const { key, namespace = "" } = params;
-        if (isObject(storage[namespace])) return storage[namespace][key];
-        if(storage[key]) return storage[key];
-        window.WEB_STORAGE_IS_WARNING && log("NOT_FOUND_NAME", "warn", key);
-      },
-      setItem(params: SetParams) {
-        const { key, value, namespace } = params;
-        const space = _createSpace(namespace)
-        space[key] = value
-        _update()
-      },
-      removeItem(params: RemoveParams) {
-        const { key, namespace="" } = params;
-        if (isObject(storage[namespace])){
-          delete storage[namespace][key]
-        }else{
-          delete storage[key]
-        }
-        _update()
-      },
-      removeSpace(space: string) {
-        delete storage[space];
-        _update();
-      },
-      clear() {
-        for (let key in storage) {
-          delete storage[key];
-        }
-        _update();
+
+  return new Proxy(
+    {
+      methods: {
+        getItem(params: GetParams) {
+          const { key, namespace = "" } = params;
+          const storage = load();
+          if (isObject(storage[namespace])) return storage[namespace][key];
+          if (storage[key]) return storage[key];
+          window.WEB_STORAGE_IS_WARNING && log("NOT_FOUND_NAME", "warn", key);
+        },
+        setItem(params: SetParams) {
+          const { key, value, namespace } = params;
+          const storage = load();
+          const space = _createSpace(storage, namespace);
+          space[key] = value;
+          _update(storage);
+        },
+        removeItem(params: RemoveParams) {
+          const { key, namespace = "" } = params;
+          const storage = load();
+          if (isObject(storage[namespace])) {
+            delete storage[namespace][key];
+          } else {
+            delete storage[key];
+          }
+          _update(storage);
+        },
+        removeSpace(space: string) {
+          const storage = load();
+          delete storage[space];
+          _update(storage);
+        },
+        clear() {
+          const storage = load();
+          for (let key in storage) {
+            delete storage[key];
+          }
+          _update(storage);
+        },
       },
     },
-  };
+    {
+      get(target, name) {
+        if (name === "storage") {
+          return load();
+        }
+        return target;
+      },
+    }
+  );
 }
